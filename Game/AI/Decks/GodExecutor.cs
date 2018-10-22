abae8e60fbfb8ee15cf1364a2966514b02bbd810
Przemysław Adamczyk <przemyslaw.adamczyk@microsoft.wsei.edu.pl>
2018-10-22 22:22:59 +0200

    potential draw added

--- Game/AI/Decks/GodExecutor.cs
+++ Game/AI/Decks/GodExecutor.cs
@@ -11,6 +11,8 @@ namespace WindBot.Game.AI.Decks
         private bool wasMakyuraUsedThisTurn=false;
         private int MakyuraCount = 0;
         private bool chickenGameCantDraw = false;
+        //potential draw in one chain.
+        private int potentialDraw = 0;
         
         public class CardId
         {
@@ -39,13 +41,13 @@ namespace WindBot.Game.AI.Decks
         {
          
         // Cycle begins
-            AddExecutor(ExecutorType.Activate, CardId.UpstartGoblin);
+            AddExecutor(ExecutorType.Activate, CardId.UpstartGoblin, UpstartGoblinEffect);
             AddExecutor(ExecutorType.Activate, CardId.PainfulChoice, PainfulChoiceEffect);
             AddExecutor(ExecutorType.Activate, CardId.PotOfGreed, PotOfGreedEffect);
             AddExecutor(ExecutorType.Activate, CardId.PotOfDuality, PotOfDualityEffect);
             AddExecutor(ExecutorType.Activate, CardId.ChickenGame, ChickenGameField);
             AddExecutor(ExecutorType.Activate, CardId.ChickenGame, ChickenGameEffect);
-            AddExecutor(ExecutorType.Activate, CardId.OneDayOfPeace);
+            AddExecutor(ExecutorType.Activate, CardId.OneDayOfPeace, OneDayOfPeaceEffect);
             AddExecutor(ExecutorType.Activate, CardId.GracefulCharity, GracefulCharityEffect);
             // Power Overwhelming
             AddExecutor(ExecutorType.Activate, CardId.RecklessGreed, RecklessGreedEffect);
@@ -54,6 +56,24 @@ namespace WindBot.Game.AI.Decks
             AddExecutor(ExecutorType.Activate, CardId.MagicalMallet, MagicalMalletEffect);
             AddExecutor(ExecutorType.Activate, CardId.JarOfAvarice, JarOfAvariceEffect);
         }
+
+        private bool OneDayOfPeaceEffect()
+        {
+            updatePotentialDraw(1);
+            return true;
+        }
+
+        private bool UpstartGoblinEffect()
+        {
+            updatePotentialDraw(1);
+            return true;
+        }
+
+        private void updatePotentialDraw(int draw)
+        {
+            potentialDraw+=draw;
+        }
+
         private int[] ExodiaPieces = { CardId.Exodia, CardId.RightArm, CardId.RightLeg, CardId.LeftLeg, CardId.LeftArm };
 
         private bool ChickenGameField()
@@ -69,6 +89,7 @@ namespace WindBot.Game.AI.Decks
 
         private bool ChickenGameEffect()
         {
+            updatePotentialDraw(1);
             if (!wasChickenGameActivated&&Card.Location==CardLocation.SpellZone && Bot.LifePoints>1000 && ActivateDescription == AI.Utils.GetStringId(CardId.ChickenGame, 0))
             {
                 AI.SelectOption(0);
@@ -80,6 +101,7 @@ namespace WindBot.Game.AI.Decks
                 wasChickenGameActivated = true;
                 chickenGameCantDraw = true;
             }
+            updatePotentialDraw(-1);
             return false;
         }
 
@@ -90,31 +112,39 @@ namespace WindBot.Game.AI.Decks
 
         private bool PotOfGreedEffect()
             {
-            return Bot.Deck.Count > 1;
+            updatePotentialDraw(2);
+            if(Bot.Deck.Count >= potentialDraw)
+            {
+                return true;
+            }
+            updatePotentialDraw(-2);
+            return false;
             }
 
 
         //To rethink!
         private bool GracefulCharityEffect()
         {
-            if (Bot.Deck.Count > 2)
-            {   
-                 AI.SelectCard(
-                CardId.Makyura,
-                CardId.SixthSense,
-                CardId.HopeForEscape,
-                CardId.JarOfAvarice,
-                CardId.RecklessGreed,
-                CardId.OneDayOfPeace,
-                CardId.ChickenGame,
-                CardId.UpstartGoblin,
-                CardId.PainfulChoice,
-                CardId.MagicalMallet
-                
-                );
-            return true;
+            updatePotentialDraw(3);
+            if (Bot.Deck.Count >= potentialDraw)
+            {
+                AI.SelectCard(
+               CardId.Makyura,
+               CardId.SixthSense,
+               CardId.HopeForEscape,
+               CardId.JarOfAvarice,
+               CardId.RecklessGreed,
+               CardId.OneDayOfPeace,
+               CardId.ChickenGame,
+               CardId.UpstartGoblin,
+               CardId.PainfulChoice,
+               CardId.MagicalMallet
+
+               );
+                return true;
             }
-            else return false;
+            updatePotentialDraw(-3);
+            return false; 
         }
         private int ExodiaPiecesInHand()
         {
@@ -130,6 +160,7 @@ namespace WindBot.Game.AI.Decks
         }
         private bool PotOfDualityEffect()
         {
+            updatePotentialDraw(1);
             if (ExodiaPiecesInHand()==4)
             {
                 AI.SelectCard(
@@ -218,6 +249,7 @@ namespace WindBot.Game.AI.Decks
                 MakyuraCount = MakyuraGraveyardCount();
                 wasMakyuraUsedThisTurn = true;
             }
+            potentialDraw = 0;
             base.OnChainEnd();
         }
         public override void OnChaining(int player, ClientCard card)
@@ -241,8 +273,9 @@ namespace WindBot.Game.AI.Decks
         
         private bool PainfulChoiceEffect()
         {
+            updatePotentialDraw(5);
             Logger.WriteErrorLine("PainfulChoice targets in deck:"+TargetsForPainfulChoise().ToString());
-            if (Bot.Deck.Count > 5 && TargetsForPainfulChoise()>5)
+            if (Bot.Deck.Count > potentialDraw && TargetsForPainfulChoise()> potentialDraw)
             {
                 AI.SelectCard(
                 CardId.Makyura,
@@ -259,22 +292,26 @@ namespace WindBot.Game.AI.Decks
                 );
             return true;
             }
+            updatePotentialDraw(-5);
             return false;
         }
 
         private bool RecklessGreedEffect()
         {
-            if (AI.Utils.GetLastChainCard() == null && Bot.Deck.Count >1)
+            updatePotentialDraw(2);
+            if (Bot.Deck.Count >=potentialDraw&&wasMakyuraUsedThisTurn)
             {
-                return wasMakyuraUsedThisTurn;
+                return true;
             }
+            updatePotentialDraw(-2);
             return false;
         }
 
         private bool HopeForEscapeEffect()
         {
-
-            if (Bot.Deck.Count >=((AI.Utils.Enemy.LifePoints - (Bot.LifePoints-1000))/2000) && Bot.LifePoints>1000 && !Bot.HasInHand(CardId.UpstartGoblin)&&!Bot.HasInHand(CardId.ChickenGame) && !AI.Utils.ChainContainsCard(CardId.UpstartGoblin)&&!AI.Utils.ChainContainsCard(CardId.ChickenGame)&& !AI.Utils.ChainContainsCard(CardId.PainfulChoice))
+            int draw = (AI.Utils.Enemy.LifePoints - (Bot.LifePoints - 1000)) / 2000;
+             updatePotentialDraw(draw);
+            if (wasMakyuraUsedThisTurn && Bot.Deck.Count >=potentialDraw && Bot.LifePoints>1000 && !Bot.HasInHand(CardId.UpstartGoblin)&&!Bot.HasInHand(CardId.ChickenGame) && !AI.Utils.ChainContainsCard(CardId.UpstartGoblin)&&!AI.Utils.ChainContainsCard(CardId.ChickenGame)&& !AI.Utils.ChainContainsCard(CardId.PainfulChoice))
             {
                 if (Bot.HasInSpellZone(CardId.ChickenGame) && (!wasChickenGameActivated || chickenGameCantDraw))
                 {
@@ -287,17 +324,19 @@ namespace WindBot.Game.AI.Decks
                 }
                 else if (AI.Utils.ChainContainsCard(CardId.HopeForEscape) && (difference < 1000))
                 {
-                    
+                    updatePotentialDraw(-draw);
                     return false;
                 }
-                    return wasMakyuraUsedThisTurn;  
+                    return true;
             }
-            else return false;
+            updatePotentialDraw(-draw);
+            return false;
         }
 
         private bool SixthSenseEffect()
         {
-            if (Bot.Deck.Count > 5 && AI.Utils.GetLastChainCard()==null)
+            updatePotentialDraw(6);
+            if (Bot.Deck.Count > potentialDraw)
             {
                 if (wasMakyuraUsedThisTurn && Bot.HasInHand(CardId.JarOfAvarice))
                 {
@@ -306,13 +345,16 @@ namespace WindBot.Game.AI.Decks
                    AI.SelectNextNumber(5);
                     return true;
                 }
-                else return false;
+                updatePotentialDraw(-6);
+                return false;
             }
-            else return false;
+            updatePotentialDraw(-6);
+            return false;
         }
         //To Rethink!
         private bool JarOfAvariceEffect()
         {
+            updatePotentialDraw(-4);
             if (AI.Utils.ChainContainsCard(CardId.SixthSense) || AI.Utils.ChainContainsCard(CardId.PainfulChoice)) { return false; }
             if (wasMakyuraUsedThisTurn&&Bot.HasInGraveyard(CardId.Exodia)||Bot.HasInGraveyard(CardId.LeftArm)||Bot.HasInGraveyard(CardId.RightArm)||Bot.HasInGraveyard(CardId.LeftLeg)||Bot.HasInGraveyard(CardId.RightLeg))
                 {
@@ -332,7 +374,8 @@ namespace WindBot.Game.AI.Decks
                 );
                 return true;
                 }
-            else return false;
+            updatePotentialDraw(4);
+            return false;
         }
         
         
